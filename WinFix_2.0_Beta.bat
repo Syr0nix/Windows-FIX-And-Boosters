80e0054c737cbac263318645d66597f0b0d90bd4
Syr0nix <110576731+Syr0nix@users.noreply.github.com>
2024-06-10 02:42:34 -0400

    Update WinFix_2.0_Beta.bat

--- WinFix_2.0_Beta.bat
+++ WinFix_2.0_Beta.bat
@@ -15,7 +15,7 @@ exit /b
 :start
 setlocal enabledelayedexpansion
 for %%i in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
-    echo Checking drive %%i: - procedure 1 of 8
+    echo Checking drive %%i: - procedure 1 of 9
     if exist %%i:\ (
         echo Drive %%i: is available, running CHKDSK now...
         chkdsk %%i: /scan
@@ -30,26 +30,26 @@ for %%i in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
 )
 endlocal
 
-echo Cleaning Temp and Tmp folders - procedure 2 of 8
+echo Cleaning Temp and Tmp folders - procedure 2 of 9
 DEL /F /S /Q %Temp%\*.*
 DEL /F /S /Q %Tmp%\*.*
 rd %temp% /s /q
 del /q/f/s %TEMP%*
 
-echo Windows component files check - procedure 3 of 8
+echo Windows component files check - procedure 3 of 9
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 Dism.exe /online /Cleanup-Image /SPSuperseded
 
-echo Checking the integrity of the Windows image - procedure 4 of 8
+echo Checking the integrity of the Windows image - procedure 4 of 9
 DISM /Online /Cleanup-Image /CheckHealth
 DISM /Online /Cleanup-Image /ScanHealth
 DISM /Online /Cleanup-Image /RestoreHealth
 
-echo Running System file check - procedure 5 of 8
+echo Running System file check - procedure 5 of 9
 sfc /scannow
 echo If SFC found some errors and could not repair, re-run the script after a reboot.
 
-echo Modifying network settings - procedure 6 of 8
+echo Modifying network settings - procedure 6 of 9
 
 echo This script is modifying some network settings.
 netsh interface tcp set global autotuning=high
@@ -97,7 +97,7 @@ Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "SackOp
 Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "DefaultTTL" /t REG_DWORD /d "64" /f
 Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "NetworkThrottlingIndex" /t REG_SZ /d "ffffffff" /f
 echo.
-echo procedure 7 of 8
+echo procedure 7 of 9
 echo "Releasing the current IP address..." 
 ipconfig /release
 timeout 5
@@ -107,6 +107,7 @@ timeout 5
 echo "Flushing the DNS resolver cache..."
 ipconfig /flushdns
 timeout 5
-echo Cleaning Prefetch files to speed up the Windows boot process - procedure 8 of 8
+echo Updating System Programs And Drivers - procedure 8 of 9
+echo Cleaning Prefetch files to speed up the Windows boot process - procedure 9 of 9
 del /q/f/s %WINDIR%\Prefetch\*.*
 pause Press any key to close this program...
