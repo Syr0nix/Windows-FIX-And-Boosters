d431b524da50555568f56402d160bd0f307ef09b
Syr0nix <110576731+Syr0nix@users.noreply.github.com>
2024-06-10 01:54:59 -0400

    Update WinFix_2.0_Beta.bat

--- WinFix_2.0_Beta.bat
+++ WinFix_2.0_Beta.bat
@@ -3,6 +3,20 @@ color 0A
 title WinFix script
 
 echo This script was made by Syr0nix.
+
+echo    ______                        ______             __                 
+echo   /      \                      /      \           /  |                
+echo  /$$$$$$  | __    __   ______  /$$$$$$  | _______  $$/  __    __       
+echo  $$ \__$$/ /  |  /  | /      \ $$$  \$$ |/       \ /  |/  \  /  |      
+echo  $$      \ $$ |  $$ |/$$$$$$  |$$$$  $$ |$$$$$$$  |$$ |$$  \/$$/       
+echo   $$$$$$  |$$ |  $$ |$$ |  $$/ $$ $$ $$ |$$ |  $$ |$$ | $$  $$<        
+echo  /  \__$$ |$$ \__$$ |$$ |      $$ \$$$$ |$$ |  $$ |$$ | /$$$$  \       
+echo  $$    $$/ $$    $$ |$$ |      $$   $$$/ $$ |  $$ |$$ |/$$/ $$  |      
+echo   $$$$$$/   $$$$$$$ |$$/        $$$$$$/  $$/   $$/ $$/ $$/   $$/       
+echo            /  \__$$ |                                                  
+echo            $$    $$/                                                   
+echo             $$$$$$/                                                    
+
 echo This script needs to be run as an administrator. It will attempt to fix common Windows issues And Speed up GamePlay.
 echo Are you sure you want to proceed? (Y/N)
 set /p userinp=Choice: 
